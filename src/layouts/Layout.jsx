6bcc4b052432c014a2ba30bc37781945ad7a47cd
Lillegul <camillal1@live.no>
2025-10-24 14:12:05 +0200

    fix map and fonts

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -5,11 +5,8 @@ import Footer from '../components/footer.jsx'
 function Layout() {
   return (<>
   <Header />
-    <h1>Layout</h1>
-   
-  <Outlet /> 
+  <Outlet />
   <Footer />
-    
    </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,14 @@ function Home() {
       <p>Bli med på åpningen av Atmos kunstkollektiv i Thomas Angells gt. 27 gate, 1. november kl. 10:00.</p>
       <p>Kom innom for en koselig atmosfære med stor variasjon av håndlagde produkter fra lokale kunstnere!</p>
       {/* <Link to="/designers">designers</Link> */}
-      <div>
+      
+      <div className="Åpningsider">
+        <h2>Åpningstider etter 1. November</h2>
+        <p>
+          Mandag-lørdag: 10:00-18:00</p>
+
+      </div>
+<div>
         <h2>Hvor finner du oss?</h2>
         <p>Du finner oss i den gamle butikken til M. Sommer, ved siden av Flying tiger. </p>
         <MapComponent />
@@ -23,14 +30,6 @@ function Home() {
 <img className="locationPic" src={LocationPic}/>
 </a > */}
       </div>
-
-      <div className="Åpningsider">
-        <h2>Åpningstider</h2>
-        <p>
-          Mandag-lørdag: 10:00-18:00</p>
-
-      </div>
-
 
       <p>Denne nettsiden er under utvikling :) </p>
     </main>
