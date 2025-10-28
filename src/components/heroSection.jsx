0a22edd34c5d9b4f0eeebb83dfee345833c3db36
Lillegul <camillal1@live.no>
2025-10-28 09:41:50 +0100

    add banner to desktop

--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -1,11 +1,12 @@
 import HeroImg from '../assets/openingBanner.png'
 import '../styles/hero.css'
+import bannerDesktop from '../assets/bannerDesktop.png'
 
 
 function HeroSection() {
   return <>
   <img className="heroImg" src={HeroImg} ></img>
-  
+  <img className="heroImgDesktop" src={bannerDesktop} ></img>
   </>
 }
 
